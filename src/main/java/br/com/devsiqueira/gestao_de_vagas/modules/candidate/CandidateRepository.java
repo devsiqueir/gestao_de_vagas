a77ca981ec98a47074063553aa62c8f7738ea99b
rsiqueira011 <rsiqueira011>
2024-04-26 16:56:29 -0300

    Validating existing user entity:candidate

---
+++ src/main/java/br/com/devsiqueira/gestao_de_vagas/modules/candidate/CandidateRepository.java
@@ -0,0 +1,10 @@
+package br.com.devsiqueira.gestao_de_vagas.modules.candidate;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
+    Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
+}
